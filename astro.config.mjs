cd676f29200a45e8fe0030a0f0b6022d83ecb43e
Paul Scanlon <pauliescanlon@gmail.com>
2024-03-03 16:38:52 -0500

    feat: add counter example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
+import qwikdev from '@qwikdev/astro';
 
-export default defineConfig({});
+export default defineConfig({
+  integrations: [qwikdev()],
+});
